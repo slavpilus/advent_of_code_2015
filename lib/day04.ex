18a97ee871c22db8987a5bf37e5ce1f90ecb8ed2
Slav Pilus <slav@pilus.me.uk>
2025-11-29 06:53:57 +0000

    day 4 part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,25 +1,26 @@
 defmodule AOC.Day04 do
   def part1(input) do
-    {_, counter} = find_hash(String.trim(input), 0)
+    {_, counter} = find_hash(String.trim(input), 0, "00000")
     counter
   end
 
-  def part2(_input) do
-    {:OK, 1}
+  def part2(input) do
+    {_, counter} = find_hash(String.trim(input), 0, "000000")
+    counter
   end
 
-  def find_hash(key, counter) do
+  def find_hash(key, counter, gold) do
     full_key = key <> Integer.to_string(counter)
 
     hash_found =
       :crypto.hash(:md5, full_key)
       |> Base.encode16(case: :lower)
-      |> String.starts_with?("00000")
+      |> String.starts_with?(gold)
 
     if hash_found do
       {:done, counter}
     else
-      find_hash(key, counter + 1)
+      find_hash(key, counter + 1, gold)
     end
   end
 end
--- test/day04_test.exs
+++ test/day04_test.exs
@@ -5,10 +5,4 @@ defmodule AOC.Day04Test do
     assert AOC.Day04.part1("abcdef") == 609_043
     assert AOC.Day04.part1("pqrstuv") == 1_048_970
   end
-
-  # test "part2" do
-  # assert AOC.Day04.part2("^>") == 3
-  # assert AOC.Day04.part2("^>v<") == 3
-  # ssert AOC.Day04.part2("^v^v^v^v^v") == 11
-  # end
 end
